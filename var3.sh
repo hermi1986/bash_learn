0607491b6ba00a0e491cfd70ac709a72cec623cc
Johannes <johannes.herrmann@web.de>
2017-02-16 11:35:54 +0100

    Drittes Variablenskript

---
+++ var3.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Ich wurde mit ${#} Parametern aufgerufen"
+echo "Mein Name ist ${0}"
+echo "Mein erster Parameter ist ${1}"
+echo "Mein zweiter Parameter ist ${2}"
+echo "Alle Parameter sind ${@}"
