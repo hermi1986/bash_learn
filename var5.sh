0a14d19fac35fddfe5e809b6fd7749d706636040
Johannes <johannes.herrmann@web.de>
2017-02-16 14:38:12 +0100

    Aktualisierungen

--- var5.sh
+++ var5.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 echo -en "Wie lautet Ihr Name, [ `whoami` ] ? "
 read myname
-if [ -z "${myname}" ]; then
-	myname=`whoami`
-fi
-echo "Ihr Name ist: ${myname}"
+echo "Ihr Name ist: ${myname:-`whoami`}"
